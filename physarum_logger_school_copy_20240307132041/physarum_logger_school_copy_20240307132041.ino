a2d146ac0324779fcee9b189b205a856e7160ed9
Brett Ian Balogh <giantmolecules@gmail.com>
2024-03-07 13:21:15 -0600

    zzz

---
+++ physarum_logger_school_copy_20240307132041/physarum_logger_school_copy_20240307132041.ino
@@ -0,0 +1,244 @@
+//----{INCLUDES}------------------------------------------------------//
+#include "Adafruit_MQTT.h"
+#include "Adafruit_MQTT_Client.h"
+#include <WiFi.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_ST7789.h>
+#include <Wire.h>
+#include <esp_task_wdt.h>
+//#include "bitmap.h"
+#include <time.h>
+
+//----{DEFINES}------------------------------------------------------//
+
+#define WLAN_SSID       "SAIC-Guest"
+#define WLAN_PASS       "wifi@saic"
+#define AIO_SERVER      "io.adafruit.com"
+#define AIO_SERVERPORT  1883                   // use 8883 for SSL
+#define AIO_USERNAME    ""
+#define AIO_KEY         ""
+
+#define WDT_TIMEOUT 15
+
+#define BUFLEN 100
+
+//----{INSTANCES}------------------------------------------------------//
+
+WiFiClient client;
+
+Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
+
+Adafruit_MQTT_Publish physarum = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/physarum");
+
+Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST);
+
+//----{VARIABLES}------------------------------------------------------//
+
+const char* ntpServer = "pool.ntp.org";
+const long  gmtOffset_sec = -21600;
+const int   daylightOffset_sec = 3600;
+
+
+
+int time_now = 0;
+int time_then = 0;
+int pubInterval = 3000;
+bool pubNow = 0;
+
+int physarumFilterBuffer[BUFLEN];
+
+int phyReading = 0;
+int avgPhysarumReading = 0;
+
+//----{SETUP}------------------------------------------------------//
+
+void setup() {
+
+  // Set up serial comms
+  Serial.begin(115200);
+
+  // Setup WDT
+  Serial.println("Configuring WDT...");
+  esp_task_wdt_init(WDT_TIMEOUT, true); //enable panic so ESP32 restarts
+  esp_task_wdt_add(NULL); //add current thread to WDT watch
+
+  // Setup WiFi
+  Serial.println(); Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(WLAN_SSID);
+  WiFi.begin(WLAN_SSID, WLAN_PASS);
+  Serial.print("Connecting to network");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+  Serial.println("WiFi connected");
+  Serial.println("IP address: "); Serial.println(WiFi.localIP());
+
+  // Setup TFT
+  // turn on backlite
+  pinMode(TFT_BACKLITE, OUTPUT);
+  digitalWrite(TFT_BACKLITE, HIGH);
+  // turn on the TFT / I2C power supply
+  pinMode(TFT_I2C_POWER, OUTPUT);
+  digitalWrite(TFT_I2C_POWER, HIGH);
+  delay(10);
+
+  // initialize TFT
+  tft.init(135, 240); // Init ST7789 240x135
+  tft.setRotation(3);
+  tft.fillScreen(ST77XX_BLACK);
+  Serial.println(F("Initialized"));
+
+  // Draw background and #
+  //tft.drawBitmap(0, 0, epd_bitmap_psilocybe, 240, 135, ST77XX_YELLOW, ST77XX_BLACK);
+  tft.setTextColor(ST77XX_WHITE, ST77XX_BLACK);
+  tft.setTextSize(3);
+  tft.setCursor(0, 0);
+  tft.println("2");
+  tft.setTextSize(2);
+
+  // Initialize Filter Buffers
+  for (int i = 0; i < BUFLEN; i++) {
+    physarumFilterBuffer[i] = analogRead(18);
+    delay(10);
+  }
+
+  //Config Time
+  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+}
+
+//----{LOOP}------------------------------------------------------//
+
+void loop() {
+
+  // Pet the dog
+  esp_task_wdt_reset();
+
+  // Take sensor readings
+  phyReading = analogRead(8);
+
+  // Filter sensor readings
+  avgPhysarumReading = updatePhysarumFilterBuffer(phyReading);
+
+  // Kep MQTT Live
+  MQTT_connect();
+
+  // Print stats to TFT
+  updateTFT();
+
+  // Publish everything
+  time_now = millis();
+  if (time_now - time_then > pubInterval || pubNow == 1) {
+    statsPublish();
+    time_then = millis();
+    pubNow = 0;
+  }
+
+}
+
+//----{FUNCTIONS}------------------------------------------------------//
+
+int updatePhysarumFilterBuffer(int val) {
+
+  // shift values in the buffer
+  for (int i = 0; i < BUFLEN - 1; i++) {
+    physarumFilterBuffer[i] = physarumFilterBuffer[i + 1];
+    //read the analog value from pin 18 (A0)
+  }
+  physarumFilterBuffer[BUFLEN - 1] = val;
+  int sum = 0;
+
+  for (int i = 0; i < BUFLEN; i++) {
+    sum += physarumFilterBuffer[i];
+  }
+
+  int filteredValue = sum / BUFLEN;
+  //constrain(filteredValue, 300, 1100);
+  return filteredValue;
+}
+
+
+
+//----{}------------------------------------------------------//
+
+void updateTFT() {
+
+  tft.setTextColor(ST77XX_CYAN, ST77XX_BLACK);
+
+  tft.setCursor(140, 0);
+  tft.print(String(avgPhysarumReading));
+  tft.println(" ");
+
+}
+
+//----{}------------------------------------------------------//
+
+void statsPublish() {
+  Serial.println();
+  Serial.println("----{TIME}----------------");
+  Serial.println();
+  printLocalTime();
+  Serial.println();
+
+  Serial.println();
+  Serial.println("----{READINGS}----------------");
+  Serial.println();
+    // Pub physarum State
+  physarumPublish();
+  Serial.println();
+}
+
+//----{}------------------------------------------------------//
+
+void physarumPublish() {
+
+  if (! physarum.publish(avgPhysarumReading)) {
+    Serial.println(F("Physarum Failed"));
+  } else {
+    Serial.print(F("Physarum: "));
+    Serial.println(avgPhysarumReading);
+  }
+}
+
+//----{}------------------------------------------------------//
+
+void MQTT_connect() {
+  int8_t ret;
+
+  // Stop if already connected.
+  if (mqtt.connected()) {
+    return;
+  }
+
+  Serial.print("Connecting to MQTT...");
+
+  uint8_t retries = 3;
+  while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
+    Serial.println(mqtt.connectErrorString(ret));
+    Serial.println("Retrying MQTT connection in 5 seconds...");
+    mqtt.disconnect();
+    delay(5000);  // wait 5 seconds
+    retries--;
+    if (retries == 0) {
+      // basically die and wait for WDT to reset me
+      ESP.restart();
+      //while (1);
+    }
+  }
+  Serial.println("MQTT Connected!");
+}
+
+//----{}------------------------------------------------------//
+
+void printLocalTime() {
+  struct tm timeinfo;
+  if (!getLocalTime(&timeinfo)) {
+    Serial.println("Failed to obtain time");
+    return;
+  }
+  Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
+}
+
+//----{EOF}------------------------------------------------------//
